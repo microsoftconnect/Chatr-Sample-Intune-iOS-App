457b8283142b98e7e3ec77725e225244bb86d36f
Mesert Kebed <mkebede@mit.edu>
2018-07-18 17:38:47 -0400

    Use ADAL git submodule at 2.6.3

---
+++ chatr/chatTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  chatTableViewCell.swift
+//  chatr
+//
+//  Created by Meseret  Kebede on 05/07/2018.
+//  Copyright © 2018 Microsoft Intune. All rights reserved.
+//
+
+import UIKit
+
+class chatTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
