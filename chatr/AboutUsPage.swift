5b0c9951f5c56687b520e3d4dd24f7bed66bfbeb
Wilson Spearman <wilsonspearman@gmail.com>
2019-01-30 12:11:22 -0500

    Removed outdated comments

--- chatr/AboutUsPage.swift
+++ chatr/AboutUsPage.swift
@@ -37,12 +37,6 @@ class AboutUsPage: UIViewController, UITextViewDelegate, IntuneMAMPolicyDelegate
 
     /*
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
     */
 
 }
