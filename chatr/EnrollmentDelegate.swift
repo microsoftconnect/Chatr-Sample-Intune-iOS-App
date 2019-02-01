3deea777ea92b8ab96f84c55d81543e7c36bf31a
Wilson Spearman <wilsonspearman@gmail.com>
2019-02-01 15:01:42 -0500

    Added handling for case without presentingViewController

--- chatr/EnrollmentDelegate.swift
+++ chatr/EnrollmentDelegate.swift
@@ -44,7 +44,13 @@ class EnrollmentDelegateClass: NSObject, IntuneMAMEnrollmentDelegate {
             
             let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
             let chatPage = storyboard.instantiateViewController(withIdentifier: "ChatPage")
-            presentingViewController!.present(chatPage, animated: false, completion: nil)
+            if nil != self.presentingViewController {
+                self.presentingViewController!.present(chatPage, animated: false, completion: nil)
+            } else {
+                print("Warning: EnrollmentDelegate initialized without a view controller before attempting enrollment.")
+                UIUtils.getCurrentViewController().present(chatPage, animated: false, completion: nil)
+            }
+            
         } else {
             //In the case of a failure, log failure error status and code
             print("Enrollment result for identity \(status.identity) with status code \(status.statusCode)")
@@ -54,8 +60,13 @@ class EnrollmentDelegateClass: NSObject, IntuneMAMEnrollmentDelegate {
             let alert = UIAlertController(title: "Error Authenticating", message: "There was an error while logging you into you account. Please check your log in credentials and try again.", preferredStyle: .alert)
             let closeAlert = UIAlertAction.init(title: "OK", style: .default, handler: nil)
             alert.addAction(closeAlert)
+            if nil != self.presentingViewController {
+                    self.presentingViewController!.present(alert, animated: true, completion: nil)
+            } else {
+                print("Warning: EnrollmentDelegate initialized without a view controller before attempting enrollment.")
+                UIUtils.getCurrentViewController().present(alert, animated: true, completion: nil)
+            }
             
-            presentingViewController!.present(alert, animated: true, completion: nil)
         }
     }
     
